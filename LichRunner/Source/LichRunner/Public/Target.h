b9304e34b5ba468939bd2142fed7bace04b9bb77
Allosius27 <yanis.queroix@orange.fr>
2023-05-11 23:12:12 +0200

    [Prog] Add Regions

--- LichRunner/Source/LichRunner/Public/Target.h
+++ LichRunner/Source/LichRunner/Public/Target.h
@@ -10,29 +10,44 @@ UCLASS()
 class LICHRUNNER_API ATarget : public AActor
 {
 	GENERATED_BODY()
-	
+
+#pragma  region Public
 public:	
 	// Sets default values for this actor's properties
 	ATarget();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+#pragma region Methods
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+#pragma endregion 
 
+#pragma  endregion
+
+#pragma  region Protected
+protected:
+
+#pragma  region UProperties
 	UPROPERTY(VisibleAnywhere)
 	class UStaticMeshComponent* StaticMesh;
 
 	UPROPERTY(EditAnywhere)
 	float MovementRange;
+#pragma endregion 
 
+#pragma region Methods
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+#pragma endregion 
+
+#pragma region UFunctions
 	UFUNCTION()
 	void OnHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
+#pragma endregion 
 
+#pragma endregion 
+
+#pragma region Private
 private:
 	FVector moveDirection;
-	
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+#pragma endregion 
 };
